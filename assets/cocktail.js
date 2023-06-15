01e4d7235fa62a409ca92e90a63608dc7e9c0bb4
GrayCoded <130934177+GrayCoded@users.noreply.github.com>
2023-06-15 16:11:12 -0300

    delete this branch

--- assets/cocktail.js
+++ assets/cocktail.js
@@ -1,27 +1,82 @@
+var cocktailDescriptionEl = document.querySelector('.description');
+var cocktailNameEl =document.querySelector('.drink-name')
+var cocktailImgEl = document.querySelector('.drink-img');
+var cocktailContainer = document.querySelector('.drink-cont')
+
+
+
+
+//var drinkIdArray = [];
+
 
 //This is the key : 9973533
-// var requestApi = 'https://www.thecocktaildb.com/api/json/v2/9973533/recent.php';
+// var requestApi = 'https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?c=Cocktail';
+//  var randomApi = 'https://www.thecocktaildb.com/api/json/v2/9973533/random&filter.php?c=Cocktail.php';
+
+
+document.body.onload = () => {
+   randomizer();
+ }
+ 
+ 
+var requestApi = 'https://www.thecocktaildb.com/api/json/v2/9973533/randomselection.php';
+ // var randomApi = 
+ var idApi = 'https://www.thecocktaildb.com/api/json/v2/9973533/lookup.php?i=11007';
+ 
+ 
+ 
+ function randomizer () {
+ 
+ 
+    fetch(requestApi)
+       .then(function (response) {
+         return response.json()
+       })
+       .then(function (data) {
+         console.log(data.drinks); 
+ 
+         let drinks = data.drinks;
+
+
+         console.log(drinks[0].strInstructions);
+         console.log(drinks[0].idDrink);
+         console.log(drinks[0].strDrink);
+         console.log(drinks[0].strDrinkThumb);
+
+         var drinkTitle = drinks[0].strDrink;
+         var drinkInst = drinks[0].strInstructions;
+         var drinkImg = drinks[0].strDrinkThumb;
+         
+         cocktailDescriptionEl.textContent = drinkInst;
+         cocktailNameEl.textConent = drinkTitle;
+         cocktailImgEl.appendChild[0] = drinkImg;
+
+         .catch(function(error) {
+            console.log(error);
+          })
+
+
+         
+        
+         
+             
+ })
+};
+
+
+
+  
+
+
 
 //Lookup a random cocktail
 // www.thecocktaildb.com/api/json/v2/9973533/random.php
 
-// Search cocktail by name
-// www.thecocktaildb.com/api/json/v2/9973533/search.php?s=margarita
-
-// List all cocktails by first letter
-// www.thecocktaildb.com/api/json/v2/9973533/search.php?f=a
-
 // Search ingredient by name
 // www.thecocktaildb.com/api/json/v2/9973533/search.php?i=vodka
 
-// Lookup full cocktail details by id
-// www.thecocktaildb.com/api/json/v2/9973533/lookup.php?i=11007
-
-// Lookup ingredient by ID
-// www.thecocktaildb.com/api/json/v2/9973533/lookup.php?iid=552
-
 // Lookup a random cocktail
-// www.thecocktaildb.com/api/json/v2/9973533/random.php
+// 
 
 // Lookup a selection of 10 random cocktails (only available to $2+ Patreon supporters)
 // www.thecocktaildb.com/api/json/v2/9973533/randomselection.php
@@ -33,45 +88,28 @@
 // www.thecocktaildb.com/api/json/v2/9973533/latest.php
 
 // Search by ingredient
-// www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=Gin
-// www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=Vodka
+// https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=
 
-// Filter by multi-ingredient (only available to $2+ Patreon supporters)
+
+// Filter by multi-ingredient
 // www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=Dry_Vermouth,Gin,Anis
 
-// Filter by alcoholic
-// www.thecocktaildb.com/api/json/v2/9973533/filter.php?a=Alcoholic
-// www.thecocktaildb.com/api/json/v2/9973533/filter.php?a=Non_Alcoholic
 
 // Filter by Category
-// www.thecocktaildb.com/api/json/v2/9973533/filter.php?c=Ordinary_Drink
-// www.thecocktaildb.com/api/json/v2/9973533/filter.php?c=Cocktail
 
-// Filter by Glass
-// www.thecocktaildb.com/api/json/v2/9973533/filter.php?g=Cocktail_glass
-// www.thecocktaildb.com/api/json/v2/9973533/filter.php?g=Champagne_flute
+// 
 
-// List the categories, glasses, ingredients or alcoholic filters
-// www.thecocktaildb.com/api/json/v2/9973533/list.php?c=list
-// www.thecocktaildb.com/api/json/v2/9973533/list.php?g=list
-// www.thecocktaildb.com/api/json/v2/9973533/list.php?i=list
+
+
 // www.thecocktaildb.com/api/json/v2/9973533/list.php?a=list
 
 
 //  Images
+
 // Drink thumbnails
+
 // Add /preview to the end of the cocktail image URL
 // /images/media/drink/vrwquq1478252802.jpg/preview (100x100 pixels)
-
-
-// Ingredient Thumbnails
-// www.thecocktaildb.com/images/ingredients/gin-Small.png
-// (100x100 pixels)
-// www.thecocktaildb.com/images/ingredients/gin-Medium.png
-// (350x350 pixels)
-// www.thecocktaildb.com/images/ingredients/gin.png
-// (700x700 pixels)
-  
 
 
 
